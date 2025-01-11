32c4742fa7b5fb222ae4df130fe1cc89b53e10ad
Kyle Ng <kgng@usc.edu>
2025-01-11 11:09:18 -0800

    init

---
+++ verify_installations.py
@@ -0,0 +1,5 @@
+import torch
+import transformers
+
+print(f"Successfully downloaded Pytorch Version: {torch.__version__}")
+print(f"Successfully downloaded Transformers Version: {transformers.__version__}")
